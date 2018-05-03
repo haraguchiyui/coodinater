4a9c7afcdd356a2ee82ad8d83077f198942a13ba
haraguchiyui <yuimaruuu.06.10@gmail.com>
2018-05-03 23:59:25 +0900

    ::

--- app/src/main/java/com/example/yui06/coodinater/ClosetActivity.java
+++ app/src/main/java/com/example/yui06/coodinater/ClosetActivity.java
@@ -1,13 +1,32 @@
 package com.example.yui06.coodinater;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ClosetActivity extends AppCompatActivity {
+
+    FloatingActionButton add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_closet);
+
+        add=(FloatingActionButton)findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(this,EditActivity.class);
+                startActivity(intent2);
+
+            }
+        };
+
+
     }
+
+
 }
