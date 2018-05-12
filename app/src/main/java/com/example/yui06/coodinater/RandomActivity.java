71a39458a80e407496e59d50b821862fc0ff6b79
haraguchiyui <yuimaruuu.06.10@gmail.com>
2018-05-12 23:35:27 +0900

    途中

--- app/src/main/java/com/example/yui06/coodinater/RandomActivity.java
+++ app/src/main/java/com/example/yui06/coodinater/RandomActivity.java
@@ -2,12 +2,62 @@ package com.example.yui06.coodinater;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.List;
+import java.util.Random;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class RandomActivity extends AppCompatActivity {
+
+    Realm realm;
+    public List<MemoActivity> mCards;
+    int number;
+
+    ImageView imageView2;
+    ImageView imageView3;
+    Button again;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random);
+
+        imageView2=(ImageView)findViewById(R.id.imageView2);
+        imageView3=(ImageView)findViewById(R.id.imageView3);
+        again=(Button)findViewById(R.id.again);
+
+
+        realm=Realm.getDefaultInstance();
+
+        RealmResults<MemoActivity> results=realm.where(MemoActivity.class).findAll();
+        List<MemoActivity> items =realm.copyFromRealm(results);
+
+        Random random=new Random();
+        number=random.nextInt(mCards.size());
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+
+        realm.close();
     }
 }
